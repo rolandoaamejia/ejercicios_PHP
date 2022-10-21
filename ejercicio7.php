27f7a03834c8df51a14a90b547b18ade24fe4534
Rolando Antonio Mejía <50997752+rolandoaamejia@users.noreply.github.com>
2022-10-21 06:57:31 -0600

    constantes y variables
    ejercicio 7 constantes y variables

--- ejercicio7.php
+++ ejercicio7.php
@@ -1,9 +1,19 @@
 <?php
- $Edad=32;
+ $Edad=32; //Variables
 
  echo $Edad."<br/>";
 
- $Edad=40;
+ $Edad=40; //Cambiar valor de la variable
 
- echo $Edad;
+ echo $Edad."<br/>";;
+
+ //Uso de constante
+ define("NOMBRE","OSCAR");
+
+ //Imprimir constante este valor no va cambiar a diferencia de las variables
+ echo NOMBRE;
+
+ NOMBRE = "Pedro";
+
+ echo NOMBRE;
 ?>
